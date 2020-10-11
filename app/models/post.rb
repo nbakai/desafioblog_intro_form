913ce6e7ddfe85f401a3c7a7227a813c8280a2f5
Melisa Budini <melixbq@gmail.com>
2020-10-11 13:22:02 -0300

    up

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,4 @@
 class Post < ApplicationRecord
-    has_many :spoiler, class_name: "Post"
     validates :content, presence: true, allow_blank: false
     validates :title, presence: true, allow_blank: false
     validates :image_url, presence: true, allow_blank: false
