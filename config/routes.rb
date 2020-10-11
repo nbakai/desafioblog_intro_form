4c506b967076819f9cbac7042c928754b508bb8f
Melisa Budini <melixbq@gmail.com>
2020-10-10 23:40:32 -0300

    update: views b.auth, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'posts#index'
+  get 'posts/dashboard', to: 'posts#dashboard'
+  post 'posts/dashboard', to: 'posts#create', as: 'posts_dash_create'
+  get 'posts/create', to: 'posts#show'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
